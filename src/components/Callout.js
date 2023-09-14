f6046174a8497f247ab4fc8dd69177d55cf4c8cb
Connor McCarl <connormccarl@gmail.com>
2023-09-14 11:56:50 -0700

    added white text option to callout text

--- src/components/Callout.js
+++ src/components/Callout.js
@@ -1,7 +1,7 @@
 function Callout(props){
     return (
         <div className={"calloutContainer " + props.color }>
-            <div className="calloutText column">{props.children}</div>
+            <div className={"calloutText column " + ( props.color === "green" || props.color === "blue" ? "white" : "")}>{props.children}</div>
             <div className="calloutButton column">
                 <button>{props.buttonText === "PDF" ? "Download PDF Here" : "Join Our Facebook Group Here"}</button>
             </div>
